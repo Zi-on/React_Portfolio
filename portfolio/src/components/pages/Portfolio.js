76bd9fe176bc52be307c59113f882b6815695e74
Zi-on <zioniflores12@yahoo.com>
2021-09-15 18:58:49 -0500

    added images to my projects and added a link to github and deployed site

--- portfolio/src/components/pages/Portfolio.js
+++ portfolio/src/components/pages/Portfolio.js
@@ -1,25 +1,56 @@
 import React from 'react';
-import Figure from 'react-bootstrap/Figure';
-import employee from '../src/pictures/employee.jpg';
+import Card from 'react-bootstrap/Card';
+import Nav from 'react-bootstrap/Nav';
+import Row from 'react-bootstrap/Row';
+
 
 
 export default function Portfoio({ projects }) {
+  
   return (
     <div>
       <h1>Portfoio</h1>
-        {projects.map((project) => (
-         <Figure>
-         <Figure.Image
-           width={171}
-           height={180}
-           alt="171x180"
-           src={employee}
-         />
-         <Figure.Caption>
-           {`${project.title} ${project.github}`}
-         </Figure.Caption>
-       </Figure>
-        ))}
+      <Row>
+      {projects.map((project) => (
+        <Card style={{ width: "18rem" }}>
+          <Card.Img variant="top" src={project.img} />
+          <Card.Body>
+            <Card.Title>{project.title}</Card.Title>
+            <Nav
+              onSelect={() => window.open(`${project.github}`)}
+            >
+              <Nav.Item>
+                <Nav.Link href="/home" src={project.git}>Github</Nav.Link>
+              </Nav.Item>
+    
+            </Nav>
+            <Nav
+              onSelect={() => window.open(`${project.deployed}`)}
+            >
+              <Nav.Item>
+                <Nav.Link href="/home" src={project.git}>Deployed Site</Nav.Link>
+              </Nav.Item>
+    
+            </Nav>
+          </Card.Body>
+        </Card>
+      ))}
+      </Row>
     </div>
   );
 }
+
+/* <div>
+<h1>Portfoio</h1>
+  {projects.map((project) => (
+<Card style={{ width: '18rem' }}>
+<Card.Img variant="top" src={project.img} />
+<Card.Body>
+ <Card.Title>{project.title}</Card.Title>
+ <Image href={project.github} src={project.git} width={40} height={40} roundedCircle />
+ <Button variant="primary">{project.deployed}</Button>
+</Card.Body>
+</Card>
+  ))}
+</div>
+); */
--- portfolio/src/components/projects.js
+++ portfolio/src/components/projects.js
@@ -1,33 +1,45 @@
+import employeeImg from '../pictures/employee.jpg';
+import cryptoImg from '../pictures/cryptocurrency.png';
+import githubEmoji from "../pictures/github.png";
+import weatherImg from "../pictures/Weather-icon.png";
+import techImg from "../pictures/tech.png";
+import sheduleImg from "../pictures/wrkday.png"
+
 const Projects = [
   {
     title: "Employee_Tracker",
     github: "https://github.com/Zi-on/Employee_Tracker",
     deployed: "N/A",
-    img: "../pictures/employee.jpg",
+    img: `${employeeImg}`,
+    git: `${githubEmoji}`
   },
   {
     title: "Crypto Cash News",
     github: "https://github.com/JManauth/CryptoCashNews",
     deployed: "https://jmanauth.github.io/CryptoCashNews/",
-    img: "",
+    img: `${cryptoImg}`,
+    git: `${githubEmoji}`
   },
   {
     title: "Weather Dashboard",
     github: "https://github.com/Zi-on/Weather_Dashboard",
     deployed: "https://zi-on.github.io/Weather_Dashboard/",
-    img: "",
+    img: `${weatherImg}`,
+    git: `${githubEmoji}`
   },
   {
     title: "TechBlog",
     github: "https://github.com/Zi-on/TechBlog",
     deployed: "https://frozen-gorge-64664.herokuapp.com/",
-    img: "",
+    img: `${techImg}`,
+    git: `${githubEmoji}`
   },
   {
     title: "Work day scheduler",
     github: "https://github.com/Zi-on/Work_day_scheduler",
     deployed: "https://zi-on.github.io/Work_day_scheduler/",
-    img: "",
+    img: `${sheduleImg}`,
+    git: `${githubEmoji}`
   },
 ];
 
