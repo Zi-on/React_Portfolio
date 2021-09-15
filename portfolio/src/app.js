84e8ce9213e82beedbb901531d2051c753ff2417
Zi-on <zioniflores12@yahoo.com>
2021-09-15 15:05:48 -0500

    added a navtabs folder that handles the navigation and a portfolio container that will render our pages

--- portfolio/src/app.js
+++ portfolio/src/app.js
@@ -1,18 +1,8 @@
 import React from 'react';
-// import Footer from './components/Footer';
-import Header from './components/Header';
-// import Navigation from './components/Navigation';
-// import Project from './components/Project';
+import PortfolioContainer from "./components/PortfolioContainer";
 
 
 // In our main App component, we are rendering only single instances of Header and Navbar and several instances of Card
-function App() {
-  return (
-    <div>
-      <Header />
-  
-    </div>
-  );
-}
+const App = () => <PortfolioContainer />
 
 export default App;
---
+++ portfolio/src/components/PortfolioContainer.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import NavTabs from './NavTabs';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
+import Home from './pages/Home';
+
+export default function PortfolioContainer() {
+    const [currentPage, setCurrentPage] = useState('Home');
+
+    const renderPage = () => {
+        if (currentPage === 'Home') {
+            return <Home />;
+        }
+        if (currentPage === 'About') {
+            return <About />;
+          }
+          if (currentPage === 'Portfolio') {
+            return <Portfolio />;
+          }
+          if (currentPage === 'Contact'){
+              return <Contact />;
+          }
+          return <Resume />;
+    };
+
+    const handlePageChange = (page) => setCurrentPage(page);
+
+    return (
+        <div>
+          {/* We are passing the currentPage from state and the function to update it */}
+          <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+          {/* Here we are calling the renderPage method which will return a component  */}
+          {renderPage()}
+        </div>
+      );
+}
