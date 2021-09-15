97b8c541a68524ce1c9154936771365a7964e4f2
Zi-on <zioniflores12@yahoo.com>
2021-09-15 16:54:03 -0500

    added links to my socials in footer.js

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -8,17 +8,27 @@ import linkedinEmoji from "../pictures/linkedin.png";
 import youtubeEmoji from "../pictures/youtube.png";
 
 function Footer() {
+        const linkedinClick = () => {
+          window.open("https://www.linkedin.com/in/zion-flores-684354214");
+        };
+        const githubClick = () => {
+            window.open("https://www.github.com/zi-on");
+          };
+        const youtubeClick = () => {
+            window.open("https://www.youtube.com/channel/UCe4tisvVYMzI1j51XRZEU0Q")
+        }
+        
   return (
     <Container>
       <Row>
         <Col xs={6} md={4}>
-          <Image href="https://www.linkedin.com/in/zion-flores-684354214/" src={linkedinEmoji} width={70} height={70} roundedCircle />
+          <Image onClick={linkedinClick} src={linkedinEmoji} width={70} height={70} roundedCircle />
         </Col>
         <Col xs={6} md={4}>
-          <Image src={githubEmoji} roundedCircle />
+          <Image onClick={githubClick} src={githubEmoji} roundedCircle />
         </Col>
         <Col xs={6} md={4}>
-          <Image src={youtubeEmoji} width={70} height={70} roundedCircle />
+          <Image onClick={youtubeClick} src={youtubeEmoji} width={70} height={70} roundedCircle />
         </Col>
       </Row>
     </Container>
